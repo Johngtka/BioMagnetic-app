4419e4ec5d70b639c5615680f31d51383a6fb960
Maciek Gorczynski <maciekg81@gmail.com>
2023-02-02 10:23:10 +0100

    refactor: reorganise imports, add static title, show translation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
+import { AppComponent } from './app.component';
 
 export function HttpLoaderFactory(httpClient: HttpClient) {
   return new TranslateHttpLoader(httpClient);
-
 }
+
+const navigatorLang = navigator.language.split('-')[0];
+const supportedLang = ['pl', 'es', 'en'];
+const lang = supportedLang.includes(navigatorLang) ? navigatorLang : 'en';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     TranslateModule.forRoot({
+      defaultLanguage: lang,
       loader: {
         provide: TranslateLoader,
         useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
-      }
-    })
+        deps: [HttpClient],
+      },
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
