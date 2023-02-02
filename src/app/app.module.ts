dec6d721423957dae8a2f0767bb5ecde6ade3485
Johngtka <79154804+Johngtka@users.noreply.github.com>
2023-02-02 10:57:57 +0100

    feat: Add basic support for i18n (#5)
    feat: Add basic support for i18n (#5)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { HttpClient, HttpClientModule } from '@angular/common/http'
 
-import { AppComponent } from './app.component';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core'
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'
+
+import { AppComponent } from './app.component'
+
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient)
+}
+
+const navigatorLang = navigator.language.split('-')[0]
+const supportedLang = ['pl', 'es', 'en']
+const lang = supportedLang.includes(navigatorLang) ? navigatorLang : 'en'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      defaultLanguage: lang,
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
