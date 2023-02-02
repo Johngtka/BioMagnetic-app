5b063e30a9a56258f9c7a4926abbc9b52a3000a5
Maciek Gorczynski <maciekg81@gmail.com>
2023-02-02 10:53:48 +0100

    refactor: format files with prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { HttpClient, HttpClientModule } from '@angular/common/http'
 
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core'
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
 
 export function HttpLoaderFactory(httpClient: HttpClient) {
-  return new TranslateHttpLoader(httpClient);
+  return new TranslateHttpLoader(httpClient)
 }
 
-const navigatorLang = navigator.language.split('-')[0];
-const supportedLang = ['pl', 'es', 'en'];
-const lang = supportedLang.includes(navigatorLang) ? navigatorLang : 'en';
+const navigatorLang = navigator.language.split('-')[0]
+const supportedLang = ['pl', 'es', 'en']
+const lang = supportedLang.includes(navigatorLang) ? navigatorLang : 'en'
 
 @NgModule({
   declarations: [AppComponent],
