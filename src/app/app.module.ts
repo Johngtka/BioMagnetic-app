12fe2ea78fe21b9e1c0a5c27b71c623b34fe955b
Johngtka <johngtka@gmail.com>
2023-02-03 17:30:21 +0100

    fix: repaired the patient component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core'
 import { TranslateHttpLoader } from '@ngx-translate/http-loader'
-
-import { OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
-
-import { Observable } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
@@ -25,11 +19,6 @@ export function HttpLoaderFactory(httpClient: HttpClient) {
 const navigatorLang = navigator.language.split('-')[0]
 const supportedLang = ['pl', 'es', 'en']
 const lang = supportedLang.includes(navigatorLang) ? navigatorLang : 'en'
-
-
-export interface User {
-  name: string;
-}
 
 @NgModule({
   declarations: [
@@ -54,28 +43,6 @@ export interface User {
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule implements OnInit {
-  myControl = new FormControl<string | User>('');
-  options: User[] = [{ name: 'Mary' }, { name: 'Shelley' }, { name: 'Igor' }];
-  filteredOptions!: Observable<User[]>;
+export class AppModule {
 
-  ngOnInit() {
-    this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map(value => {
-        const name = typeof value === 'string' ? value : value?.name;
-        return name ? this._filter(name as string) : this.options.slice();
-      }),
-    );
-  }
-
-  displayFn(user: User): string {
-    return user && user.name ? user.name : '';
-  }
-
-  private _filter(name: string): User[] {
-    const filterValue = name.toLowerCase();
-
-    return this.options.filter(option => option.name.toLowerCase().includes(filterValue));
-  }
 }
--- src/app/patient-search/patient-search.component.ts
+++ src/app/patient-search/patient-search.component.ts
@@ -1,9 +1,38 @@
-import { FormControl } from '@angular/forms';
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+export interface User {
+  name: string;
+}
 @Component({
   selector: 'app-patient-search',
   templateUrl: './patient-search.component.html',
 })
-export class PatientSearchComponent {
-  FormControl = new FormControl('')
+export class PatientSearchComponent implements OnInit {
+  myControl = new FormControl<string | User>('');
+  options: User[] = [{ name: 'Mary' }, { name: 'Shelley' }, { name: 'Igor' }, { name: 'Maria' }, { name: 'Mia' }, { name: 'John' }, { name: 'Sam' }, { name: 'Rose' }, { name: 'Mathew' }, { name: 'Patrick' }];
+  filteredOptions!: Observable<User[]>;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => {
+        const name = typeof value === 'string' ? value : value?.name;
+        return name ? this._filter(name as string) : this.options.slice();
+      }),
+    );
+  }
+
+  displayFn(user: User): string {
+    return user && user.name ? user.name : '';
+  }
+
+  private _filter(name: string): User[] {
+    const filterValue = name.toLowerCase();
+
+    return this.options.filter(option => option.name.toLowerCase().includes(filterValue));
+  }
 }
