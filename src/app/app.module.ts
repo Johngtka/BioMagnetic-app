87a0b618a127269a8b4396f0a9992037693d5ee2
Johngtka <johngtka@gmail.com>
2023-02-03 17:15:38 +0100

    feat: to repair patient-search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser'
 import { HttpClient, HttpClientModule } from '@angular/common/http'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core'
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'
+
+import { OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core'
-import { TranslateHttpLoader } from '@ngx-translate/http-loader'
 import { AppComponent } from './app.component';
 import { PatientSearchComponent } from './patient-search/patient-search.component';
 
@@ -18,8 +26,14 @@ const navigatorLang = navigator.language.split('-')[0]
 const supportedLang = ['pl', 'es', 'en']
 const lang = supportedLang.includes(navigatorLang) ? navigatorLang : 'en'
 
+
+export interface User {
+  name: string;
+}
+
 @NgModule({
-  declarations: [AppComponent, PatientSearchComponent,],
+  declarations: [
+    AppComponent, AppComponent, PatientSearchComponent,],
   imports: [
     BrowserModule,
     HttpClientModule,
@@ -33,9 +47,35 @@ const lang = supportedLang.includes(navigatorLang) ? navigatorLang : 'en'
       },
     }),
     MatAutocompleteModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule implements OnInit {
+  myControl = new FormControl<string | User>('');
+  options: User[] = [{ name: 'Mary' }, { name: 'Shelley' }, { name: 'Igor' }];
+  filteredOptions!: Observable<User[]>;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => {
+        const name = typeof value === 'string' ? value : value?.name;
+        return name ? this._filter(name as string) : this.options.slice();
+      }),
+    );
+  }
+
+  displayFn(user: User): string {
+    return user && user.name ? user.name : '';
+  }
+
+  private _filter(name: string): User[] {
+    const filterValue = name.toLowerCase();
+
+    return this.options.filter(option => option.name.toLowerCase().includes(filterValue));
+  }
+}
--- src/app/patient-search/patient-search.component.ts
+++ src/app/patient-search/patient-search.component.ts
@@ -1,9 +1,9 @@
+import { FormControl } from '@angular/forms';
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-patient-search',
-  templateUrl: './patient-search.component.html'
+  templateUrl: './patient-search.component.html',
 })
 export class PatientSearchComponent {
-
+  FormControl = new FormControl('')
 }
