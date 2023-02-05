3b3e2f214b026b64178876b1b5851b6bb138a2b3
Johngtka <johngtka@gmail.com>
2023-02-05 21:08:34 +0100

    refactor: remove unneeded cod

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,11 +14,11 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy()
   })
 
-  it(`should have as title 'Biomagnethic-App'`, () => {
-    const fixture = TestBed.createComponent(AppComponent)
-    const app = fixture.componentInstance
-    expect(app.title).toEqual('Biomagnethic-App')
-  })
+  // it(`should have as title 'Biomagnethic-App'`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent)
+  //   const app = fixture.componentInstance
+  //   expect(app.title).toEqual('Biomagnethic-App')
+  // })
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent)
