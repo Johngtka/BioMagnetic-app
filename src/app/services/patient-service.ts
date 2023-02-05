9bdf5cf593a87111dc9531a1b59e721ae599ac93
Johngtka <johngtka@gmail.com>
2023-02-05 19:41:20 +0100

    feat: add new patient service and implements this to patient search component

---
+++ src/app/services/patient-service.ts
@@ -0,0 +1,13 @@
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class PatientService {
+  constructor(private http: HttpClient) { }
+  PatientSearch() {
+    return this.http.get('http://localhost:3001/patient');
+  }
+
+}
