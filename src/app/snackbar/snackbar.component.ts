f4b25f5ce8de1d3c8c4642b49520b73bd279b25f
Maciek Gorczynski <maciekg81@gmail.com>
2023-02-12 12:44:19 +0100

    refactor: remove not needed component

--- src/app/snackbar/snackbar.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core'
-import { SnackService } from '../services/snack.service'
-@Component({
-  selector: 'app-snackbar',
-  templateUrl: './snackbar.component.html',
-  styleUrls: ['./snackbar.component.css'],
-  providers: [SnackService],
-})
-export class SnackbarComponent {}
