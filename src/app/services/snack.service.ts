81cc614810b4a910af30fa940d4f50f845ccc9df
Maciek Gorczynski <maciekg81@gmail.com>
2023-02-12 13:07:08 +0100

    feat: use material snackbar service

--- src/app/services/snack.service.ts
+++ src/app/services/snack.service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@angular/core'
 
+import { MatSnackBar } from '@angular/material/snack-bar'
+import { TranslateService } from '@ngx-translate/core'
+
+export enum SNACK_TYPE {
+  'error',
+  'success',
+  'info',
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class SnackService {
-  constructor() {}
+  constructor(
+    private matSnackBar: MatSnackBar,
+    private translateService: TranslateService,
+  ) {}
+
+  showSnackBarMessage(i18nKey: string, type: SNACK_TYPE) {
+    const action = this.translateService.instant('GENERAL.CLOSE')
+    const message = this.translateService.instant(i18nKey)
+    this.matSnackBar.open(message, action, {
+      duration: 3000,
+      panelClass: [
+        type === SNACK_TYPE.error
+          ? 'error-snackbar'
+          : type === SNACK_TYPE.info
+          ? 'info-snackbar'
+          : 'success-snackbar',
+        'login-snackbar',
+      ],
+    })
+  }
 }
