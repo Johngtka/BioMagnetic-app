262cde54f0598dccbcefd7506b17b7efd31588ed
Johngtka <johngtka@gmail.com>
2023-02-14 19:21:17 +0100

    fix: to repair patient table component

--- src/app/patient-table/patient-table.component.ts
+++ src/app/patient-table/patient-table.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core'
-
-import { PatientService } from './../services/patient-service';
+import { PatientService } from './../services/patient-service'
 @Component({
   selector: 'app-patient-table',
   templateUrl: './patient-table.component.html',
   styleUrls: ['./patient-table.component.css'],
 })
 export class PatientTableComponent {
+  dataSource!: any[]
   constructor(
     private patientService: PatientService
   ) { }
   ngOnInit(): void {
     this.patientService.getPatients().subscribe(patients => {
-
+      this.dataSource = patients
     })
   }
+  displayedColumns: string[] = ['name']
 }
