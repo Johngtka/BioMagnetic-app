0d0ffb23e5c3191e714d67ea488fbe8049461877
Johngtka <johngtka@gmail.com>
2023-02-14 18:32:01 +0100

    feat: new method with nocomplete geting patients to object is added

--- src/app/patient-table/patient-table.component.ts
+++ src/app/patient-table/patient-table.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core'
+
+import { PatientService } from './../services/patient-service'
 @Component({
   selector: 'app-patient-table',
   templateUrl: './patient-table.component.html',
   styleUrls: ['./patient-table.component.css'],
 })
-export class PatientTableComponent {}
+export class PatientTableComponent {
+  constructor(private patientService: PatientService) {}
+  ngOnInit(): void {
+    this.patientService.getPatients().subscribe((patients) => {})
+  }
+}
