94db4159ecf5248023d4c0d75357458b7c150d93
Johngtka <johngtka@gmail.com>
2023-02-16 22:43:11 +0100

    feat: base Gender icons added but no gender icon does not work

--- src/app/patient-table/patient-table.component.ts
+++ src/app/patient-table/patient-table.component.ts
@@ -4,34 +4,39 @@ import { SnackService, SNACK_TYPE } from './../services/snack.service';
 import { Patient } from '../models/patient';
 import { PatientService } from './../services/patient-service';
 @Component({
-    selector: 'app-patient-table',
-    templateUrl: './patient-table.component.html',
-    styleUrls: ['./patient-table.component.css'],
+  selector: 'app-patient-table',
+  templateUrl: './patient-table.component.html',
+  styleUrls: ['./patient-table.component.css'],
 })
 export class PatientTableComponent {
-    dataSource!: any[];
-    constructor(
-        private patientService: PatientService,
-        private snackService: SnackService,
-    ) {}
-    ngOnInit(): void {
-        this.patientService.getPatients().subscribe({
-            next: (data: Array<Patient>) => (this.dataSource = data),
-            error: (err) => {
-                this.snackService.showSnackBarMessage(
-                    'ERROR.PATIENT_TABLE_GET_PATIENTS',
-                    SNACK_TYPE.error,
-                );
-                console.log(err.message);
-            },
-        });
-    }
-    displayedColumns: string[] = [
-        'fullname',
-        'gender',
-        'email',
-        'phone',
-        'location',
-        'age',
-    ];
+  dataSource!: any[];
+  constructor(
+    private patientService: PatientService,
+    private snackService: SnackService,
+  ) { }
+  ngOnInit(): void {
+    this.patientService.getPatients().subscribe({
+      next: (data: Array<Patient>) => (this.dataSource = data),
+      error: (err) => {
+        this.snackService.showSnackBarMessage(
+          'ERROR.PATIENT_TABLE_GET_PATIENTS',
+          SNACK_TYPE.error,
+        );
+        console.log(err.message);
+      },
+    });
+  }
+  displayedColumns: string[] = [
+    'fullname',
+    'gender',
+    'email',
+    'phone',
+    'location',
+    'age',
+  ];
+  // gendersList: string[] = [
+  //   'male',
+  //   'female',
+  //   'no gender'
+  // ]
 }
