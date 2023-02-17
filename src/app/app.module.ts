27dbbd6cdb976c81495544475225b070cd1abec7
Johngtka <johngtka@gmail.com>
2023-02-17 13:44:32 +0100

    feat: age pipe and date-fns added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,55 +28,57 @@ import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { PatientTableComponent } from './patient-table/patient-table.component';
+import { AgePipe } from './pipes/age.pipe';
 
 export function HttpLoaderFactory(httpClient: HttpClient) {
-    return new TranslateHttpLoader(httpClient);
+  return new TranslateHttpLoader(httpClient);
 }
 const navigatorLang = navigator.language.split('-')[0];
 const supportedLang = ['pl', 'es', 'en'];
 const lang = supportedLang.includes(navigatorLang) ? navigatorLang : 'en';
 
 const materialModules = [
-    MatAutocompleteModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatIconModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatDividerModule,
-    MatSnackBarModule,
-    MatTableModule,
+  MatAutocompleteModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatIconModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatDividerModule,
+  MatSnackBarModule,
+  MatTableModule,
 ];
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        PatientSearchComponent,
-        HomeComponent,
-        AboutComponent,
-        PatientTableComponent,
-    ],
-    imports: [
-        BrowserModule,
-        HttpClientModule,
-        RouterModule,
-        BrowserAnimationsModule,
-        LayoutModule,
-        TranslateModule.forRoot({
-            defaultLanguage: lang,
-            loader: {
-                provide: TranslateLoader,
-                useFactory: HttpLoaderFactory,
-                deps: [HttpClient],
-            },
-        }),
-        FormsModule,
-        ReactiveFormsModule,
-        AppRoutingModule,
-        ...materialModules,
-    ],
-    providers: [PatientService],
-    bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+    PatientSearchComponent,
+    HomeComponent,
+    AboutComponent,
+    PatientTableComponent,
+    AgePipe,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    TranslateModule.forRoot({
+      defaultLanguage: lang,
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    ...materialModules,
+  ],
+  providers: [PatientService],
+  bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
