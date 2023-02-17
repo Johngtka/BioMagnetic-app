60108a2276c743acefe9b0d2b048fbcca2c3e782
Johngtka <johngtka@gmail.com>
2023-02-17 17:28:26 +0100

    style: formated files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,54 +31,54 @@ import { PatientTableComponent } from './patient-table/patient-table.component';
 import { AgePipe } from './pipes/age.pipe';
 
 export function HttpLoaderFactory(httpClient: HttpClient) {
-  return new TranslateHttpLoader(httpClient);
+    return new TranslateHttpLoader(httpClient);
 }
 const navigatorLang = navigator.language.split('-')[0];
 const supportedLang = ['pl', 'es', 'en'];
 const lang = supportedLang.includes(navigatorLang) ? navigatorLang : 'en';
 
 const materialModules = [
-  MatAutocompleteModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatIconModule,
-  MatButtonModule,
-  MatToolbarModule,
-  MatSidenavModule,
-  MatDividerModule,
-  MatSnackBarModule,
-  MatTableModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatSnackBarModule,
+    MatTableModule,
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PatientSearchComponent,
-    HomeComponent,
-    AboutComponent,
-    PatientTableComponent,
-    AgePipe,
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    RouterModule,
-    BrowserAnimationsModule,
-    LayoutModule,
-    TranslateModule.forRoot({
-      defaultLanguage: lang,
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient],
-      },
-    }),
-    FormsModule,
-    ReactiveFormsModule,
-    AppRoutingModule,
-    ...materialModules,
-  ],
-  providers: [PatientService],
-  bootstrap: [AppComponent],
+    declarations: [
+        AppComponent,
+        PatientSearchComponent,
+        HomeComponent,
+        AboutComponent,
+        PatientTableComponent,
+        AgePipe,
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        RouterModule,
+        BrowserAnimationsModule,
+        LayoutModule,
+        TranslateModule.forRoot({
+            defaultLanguage: lang,
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient],
+            },
+        }),
+        FormsModule,
+        ReactiveFormsModule,
+        AppRoutingModule,
+        ...materialModules,
+    ],
+    providers: [PatientService],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/pipes/age.pipe.ts
+++ src/app/pipes/age.pipe.ts
@@ -1,12 +1,10 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'age'
+    name: 'age',
 })
 export class AgePipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
-  }
-
+    transform(value: unknown, ...args: unknown[]): unknown {
+        return null;
+    }
 }
