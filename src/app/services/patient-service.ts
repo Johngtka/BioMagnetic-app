c430daec5bee0d35aba9475a9109e6f54631ff43
Johngtka <79154804+Johngtka@users.noreply.github.com>
2023-03-10 21:53:22 +0100

    feat: Add environments and connect to remote API (#23)

--- src/app/services/patient-service.ts
+++ src/app/services/patient-service.ts
@@ -4,24 +4,26 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { Patient } from '../models/patient';
+import { enviroment } from '../../environments/environment.dev';
 
 @Injectable({
     providedIn: 'root',
 })
 export class PatientService {
+    apiurl = enviroment.API_URL;
     constructor(private http: HttpClient) {}
+
     patientSearch(query: string): Observable<Array<Patient>> {
         return this.http.get<Array<Patient>>(
-            'http://localhost:3001/patient?patientName=' + query,
+            this.apiurl + '/patient?patientName=' + query,
         );
     }
+
     getPatients(): Observable<Array<Patient>> {
-        return this.http.get<Array<Patient>>('http://localhost:3001/patient');
+        return this.http.get<Array<Patient>>(this.apiurl + '/patient');
     }
+
     createPatient(patient: object) {
-        return this.http.post<Patient>(
-            'http://localhost:3001/patient',
-            patient,
-        );
+        return this.http.post<Patient>(this.apiurl + '/patient', patient);
     }
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const enviroment = {
+    production: true,
+    TITLE: 'Biomag',
+    API_URL: 'https://busy-tan-whale-tam.cyclic.app',
+};
