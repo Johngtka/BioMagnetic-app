2e4cdea1f6f63e691f8bfade2fb42836fb967372
Maciek Gorczynski <maciekg81@gmail.com>
2023-03-20 15:28:02 +0100

    feat: edit or create patient logic

--- src/app/user-input-dialog/user-input-dial.component.ts
+++ src/app/user-input-dialog/user-input-dial.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { DialogRef } from '@angular/cdk/dialog';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { Validators } from '@angular/forms';
 import { FormControl } from '@angular/forms';
@@ -7,30 +6,70 @@ import { FormControl } from '@angular/forms';
 import { SNACK_TYPE } from './../services/snack.service';
 import { SnackService } from '../services/snack.service';
 import { PatientService } from '../services/patient-service';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Patient } from '../models/patient';
 
 @Component({
     selector: 'app-user-input-dial',
     templateUrl: './user-input-dial.component.html',
     styleUrls: ['./user-input-dial.component.css'],
 })
-export class UserInputDialogComponent {
+export class UserInputDialogComponent implements OnInit {
     constructor(
         private patientService: PatientService,
         private snackService: SnackService,
-        private dialref: DialogRef,
+        public dialogRef: MatDialogRef<UserInputDialogComponent>,
+        @Inject(MAT_DIALOG_DATA) public data: { patient: Patient },
     ) {}
-    registerForm = new FormGroup({
-        name: new FormControl('', [Validators.required]),
-        surname: new FormControl('', [Validators.required]),
-        dob: new FormControl(),
-        gender: new FormControl(),
-        email: new FormControl('', [Validators.required, Validators.email]),
-        phone: new FormControl('', [
-            Validators.required,
-            Validators.pattern('^[0-9]*$'),
-        ]),
-        location: new FormControl(),
-    });
+
+    registerForm: FormGroup;
+    buttonText: string;
+    originalFormValues: Patient;
+    isEdit: boolean;
+    ngOnInit(): void {
+        if (this.data?.patient) {
+            this.isEdit = true;
+            this.buttonText = 'PATIENT_CREATE.DIALOG.UPDATE';
+            this.registerForm = new FormGroup({
+                name: new FormControl(this.data.patient.name, [
+                    Validators.required,
+                ]),
+                surname: new FormControl(this.data.patient.surname, [
+                    Validators.required,
+                ]),
+                dob: new FormControl(this.data.patient.dob),
+                gender: new FormControl(this.data.patient.gender),
+                email: new FormControl(this.data.patient.email, [
+                    Validators.required,
+                    Validators.email,
+                ]),
+                phone: new FormControl(this.data.patient.phone, [
+                    Validators.required,
+                    Validators.pattern('^[0-9]*$'),
+                ]),
+                location: new FormControl(this.data.patient.location),
+            });
+        } else {
+            this.isEdit = false;
+            this.buttonText = 'PATIENT_CREATE.DIALOG.REGISTER';
+            this.registerForm = new FormGroup({
+                name: new FormControl('', [Validators.required]),
+                surname: new FormControl('', [Validators.required]),
+                dob: new FormControl(),
+                gender: new FormControl(),
+                email: new FormControl('', [
+                    Validators.required,
+                    Validators.email,
+                ]),
+                phone: new FormControl('', [
+                    Validators.required,
+                    Validators.pattern('^[0-9]*$'),
+                ]),
+                location: new FormControl(),
+            });
+        }
+        this.originalFormValues = this.registerForm.value;
+    }
 
     get email() {
         return this.registerForm.get('email');
@@ -42,19 +81,44 @@ export class UserInputDialogComponent {
 
     addPatient() {
         const patient = this.registerForm.value;
-        this.patientService.createPatient(patient).subscribe({
-            next: () => (
-                this.snackService.showSnackBarMessage(
-                    'SUCCESS.USER_INPUT_DIALOG_CREATE_PATIENT',
-                    SNACK_TYPE.success,
+        if (this.isEdit) {
+            patient._id = this.data.patient._id;
+            this.patientService.updatePatient(patient).subscribe({
+                next: () => (
+                    this.snackService.showSnackBarMessage(
+                        'SUCCESS.USER_INPUT_DIALOG_UPDATE_PATIENT',
+                        SNACK_TYPE.success,
+                    ),
+                    this.dialogRef.close()
                 ),
-                this.dialref.close()
-            ),
-            error: () =>
-                this.snackService.showSnackBarMessage(
-                    'ERROR.USER_INPUT_DIALOG_CREATE_PATIENT',
-                    SNACK_TYPE.error,
+                error: () =>
+                    this.snackService.showSnackBarMessage(
+                        'ERROR.USER_INPUT_DIALOG_UPDATE_PATIENT',
+                        SNACK_TYPE.error,
+                    ),
+            });
+        } else {
+            this.patientService.createPatient(patient).subscribe({
+                next: () => (
+                    this.snackService.showSnackBarMessage(
+                        'SUCCESS.USER_INPUT_DIALOG_CREATE_PATIENT',
+                        SNACK_TYPE.success,
+                    ),
+                    this.dialogRef.close()
                 ),
-        });
+                error: () =>
+                    this.snackService.showSnackBarMessage(
+                        'ERROR.USER_INPUT_DIALOG_CREATE_PATIENT',
+                        SNACK_TYPE.error,
+                    ),
+            });
+        }
+    }
+
+    hasChange() {
+        return (
+            JSON.stringify(this.registerForm.value) !==
+            JSON.stringify(this.originalFormValues)
+        );
     }
 }
