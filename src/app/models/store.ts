4fee324d10f83279faadf129694600ec40dc34b2
Johngtka <johngtka@gmail.com>
2023-04-02 11:50:40 +0200

    feat: trying to add display data from /store (not working)

---
+++ src/app/models/store.ts
@@ -0,0 +1,10 @@
+export interface Store {
+    _id: string;
+    group: string;
+    id: string;
+    image: string;
+    name: string;
+    negativepoint: string;
+    positivepoint: string;
+    type: string;
+}
--- src/app/visit/visit.component.ts
+++ src/app/visit/visit.component.ts
@@ -1,25 +1,35 @@
 import { Component } from '@angular/core';
 
 import { Patient } from '../models/patient';
+// import { Store } from '../models/store';
 import { StoreService } from '../services/store.service';
+import { SnackService, SNACK_TYPE } from '../services/snack.service';
 @Component({
     selector: 'app-visit',
     templateUrl: './visit.component.html',
     styleUrls: ['./visit.component.css'],
 })
 export class VisitComponent {
-    constructor(private storeService: StoreService) {}
+    constructor(
+        private storeService: StoreService,
+        private snacService: SnackService,
+    ) {}
     patient!: Patient;
+    store;
     ngOnInit(): void {
         this.patient = history.state;
     }
     selectPatient(patientSelected: Patient) {
         this.patient = patientSelected;
         this.storeService.getStore().subscribe({
-            // next: () => (
-            // ),
-            // error: () => {
-            // },
+            next: (data) => (this.store = data),
+            error: (err) => {
+                this.snacService.showSnackBarMessage(
+                    'ERROR.PATIENT_VISIT_CREATE_PATIENT',
+                    SNACK_TYPE.error,
+                );
+                console.log(err.message);
+            },
         });
     }
 }
