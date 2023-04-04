50b61445e27fcf03ed9b920cc65ea5b74a1f7188
Johngtka <johngtka@gmail.com>
2023-04-04 21:18:44 +0200

    feat: add manual sorting in id column of visit table

--- src/app/visit/visit.component.ts
+++ src/app/visit/visit.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { MatSort } from '@angular/material/sort';
+
 import { Patient } from '../models/patient';
 import { Store } from '../models/store';
 import { StoreService } from '../services/store.service';
@@ -8,11 +11,15 @@ import { SnackService, SNACK_TYPE } from '../services/snack.service';
     templateUrl: './visit.component.html',
     styleUrls: ['./visit.component.css'],
 })
-export class VisitComponent {
+export class VisitComponent implements OnInit {
     constructor(
         private storeService: StoreService,
         private snacService: SnackService,
     ) {}
+    @ViewChild(MatSort) sort: MatSort;
+    // ngAfterViewInit(): void {
+    //     this.store.sort = this.sort;
+    // }
     patient!: Patient;
     store!: Store[];
     ngOnInit(): void {
