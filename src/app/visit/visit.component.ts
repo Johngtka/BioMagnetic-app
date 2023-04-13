e270509dddae91b28b5e94300376ae4cd8331f9e
Johngtka <johngtka@gmail.com>
2023-04-13 15:17:49 +0200

    feat: rebuild visit component

--- src/app/visit/visit.component.ts
+++ src/app/visit/visit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 import { Store } from '../models/store';
 import { Patient } from '../models/patient';
@@ -13,6 +15,8 @@ import { SnackService, SNACK_TYPE } from '../services/snack.service';
 export class VisitComponent implements OnInit {
     patient!: Patient;
     store!: Store[];
+    dataSource;
+    @ViewChild(MatPaginator) paginator: MatPaginator;
     displayedColumns: string[] = [
         'id',
         'negativePoint',
@@ -25,11 +29,16 @@ export class VisitComponent implements OnInit {
         private storeService: StoreService,
         private snackService: SnackService,
     ) {}
-
+    ngAfterViewInit() {
+        this.dataSource.paginator = this.paginator;
+    }
     ngOnInit(): void {
         this.patient = history.state;
         this.storeService.getStore().subscribe({
-            next: (data) => (this.store = data.sort((a, b) => a.id - b.id)),
+            next: (data) => (
+                (this.store = data.sort((a, b) => a.id - b.id)),
+                (this.dataSource = new MatTableDataSource<Store>(data))
+            ),
             error: (err) => {
                 this.snackService.showSnackBarMessage(
                     'ERROR.PATIENT_VISIT_CREATE_PATIENT',
