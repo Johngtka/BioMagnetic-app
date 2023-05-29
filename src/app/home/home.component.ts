eb73986544b64076bfe426ff540e8a94d65bb8ba
Johngtka <johngtka@gmail.com>
2023-05-29 12:01:03 +0200

    refactor: changed function name

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -27,7 +27,7 @@ export class HomeComponent {
             }
         });
     }
-    openVisitbySearched(patient: Patient) {
+    openVisitBySearched(patient: Patient) {
         this.route.navigate(['visit'], {
             state: patient,
         });
