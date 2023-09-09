2a6cc58741360233c7b482f296ec9e5cb91a2dd2
Johngtka <johngtka@gmail.com>
2023-09-09 11:13:08 +0200

    chore(release): v.0.0.10

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     TITLE: 'Bio-mag',
     API_URL: 'https://busy-tan-whale-tam.cyclic.app',
-    version: '0.0.0',
+    version: '0.0.10 (20230909111034)',
 };
