f972f3b266a79285c1f32e7b05108fd925a99864
Maciek Gorczynski <maciekg81@gmail.com>
2023-10-02 15:36:09 +0200

    feat: load chunks of data for store

--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -7,16 +7,16 @@ import { Store } from '../models/store';
 import { environment } from '../../environments/environment';
 import { SNACK_TYPE, SnackService } from './snack.service';
 
+interface TotalStore {
+    total: number;
+}
+
 @Injectable({
     providedIn: 'root',
 })
 export class StoreService {
-    constructor(
-        private http: HttpClient,
-        private snackService: SnackService,
-    ) {}
+    constructor(private http: HttpClient, private snackService: SnackService) {}
     store: Store[];
-    step!: number;
     apiURL = environment.API_URL;
 
     setStore(data: Store[]) {
@@ -28,29 +28,37 @@ export class StoreService {
     }
 
     fetchStore() {
-        // add logic here to loop to get all that is in the store
-        // not only the 50 as we have it now
-
         this.getAllCountOfStore().subscribe({
-            next: (data) => {
-                for (let skip = 0; skip <= data.length; skip = skip + 40) {
-                    this.step = data.length - skip;
+            next: (all) => {
+                const step = 40;
+                const total = all.total;
+                let store = [];
+                for (let skip = 0; skip <= total; skip = skip + step) {
+                    this.getStoreChunk(skip, step).subscribe({
+                        next: (data) => {
+                            store = [...store, ...data];
+                            if (store.length === total) {
+                                this.setStore(store);
+                            }
+                        },
+                        error: (err) => {
+                            this.snackService.showSnackBarMessage(
+                                'ERROR.PATIENT_VISIT_CREATE_PATIENT',
+                                SNACK_TYPE.error,
+                            );
+                            console.log(err.message);
+                        },
+                    });
                 }
             },
-        });
-        // you need to know the total that is
-        // you need to loop until the end
-        // once you are at the end you can call this.setStore()
-        this.getStoreChunk(this.step, 35).subscribe(
-            (data) => this.setStore(data),
-            (err) => {
+            error: (err) => {
                 this.snackService.showSnackBarMessage(
-                    'ERROR.PATIENT_VISIT_CREATE_PATIENT',
+                    'provide here some message that make sense',
                     SNACK_TYPE.error,
                 );
                 console.log(err.message);
             },
-        );
+        });
     }
 
     getStoreChunk(skip: number, limit: number): Observable<Array<Store>> {
@@ -58,7 +66,7 @@ export class StoreService {
             this.apiURL + `/store?skip=${skip}&limit=${limit}`,
         );
     }
-    getAllCountOfStore(): Observable<Array<Store>> {
-        return this.http.get<Array<Store>>(this.apiURL + '/store/total');
+    getAllCountOfStore(): Observable<TotalStore> {
+        return this.http.get<TotalStore>(this.apiURL + '/store/total');
     }
 }
