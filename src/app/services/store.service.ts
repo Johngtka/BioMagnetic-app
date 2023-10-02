de666c8daf49688eaf109c16d15ea629916a760e
Johngtka <johngtka@gmail.com>
2023-10-02 13:42:12 +0200

    feat: to fixed the loop the logic

--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -11,9 +11,12 @@ import { SNACK_TYPE, SnackService } from './snack.service';
     providedIn: 'root',
 })
 export class StoreService {
-    constructor(private http: HttpClient, private snackService: SnackService) {}
+    constructor(
+        private http: HttpClient,
+        private snackService: SnackService,
+    ) {}
     store: Store[];
-
+    step!: number;
     apiURL = environment.API_URL;
 
     setStore(data: Store[]) {
@@ -28,10 +31,17 @@ export class StoreService {
         // add logic here to loop to get all that is in the store
         // not only the 50 as we have it now
 
+        this.getAllCountOfStore().subscribe({
+            next: (data) => {
+                for (let skip = 0; skip <= data.length; skip = skip + 40) {
+                    this.step = data.length - skip;
+                }
+            },
+        });
         // you need to know the total that is
         // you need to loop until the end
         // once you are at the end you can call this.setStore()
-        this.getStoreChunk(0, 35).subscribe(
+        this.getStoreChunk(this.step, 35).subscribe(
             (data) => this.setStore(data),
             (err) => {
                 this.snackService.showSnackBarMessage(
@@ -47,5 +57,8 @@ export class StoreService {
         return this.http.get<Array<Store>>(
             this.apiURL + `/store?skip=${skip}&limit=${limit}`,
         );
+    }
+    getAllCountOfStore(): Observable<Array<Store>> {
+        return this.http.get<Array<Store>>(this.apiURL + '/store/total');
     }
 }
