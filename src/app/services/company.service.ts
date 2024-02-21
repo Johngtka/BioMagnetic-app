ad59c712cd1a770ab54f48c6f9273cc8b8e142a0
Johngtka <johngtka@gmail.com>
2024-02-21 18:41:21 +0100

    feat: code to check

--- src/app/services/company.service.ts
+++ src/app/services/company.service.ts
@@ -20,7 +20,7 @@ export class CompanyService {
     }
 
     getAppointments(): Observable<Appointment[]> {
-        return this.http.get<any>(this.apiURL + '/appointment1').pipe(
+        return this.http.get<any>(this.apiURL + '/appointment').pipe(
             map(
                 (appointmentFromBackend) => {
                     return appointmentFromBackend.data.map((afb) => {
